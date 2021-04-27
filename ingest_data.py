0967fddedc9455dbe0c5138a87d789fee93f878b
tinnytintin10 <tinsae.erkailo@elastic.co>
2021-04-27 09:25:58 -0400

    changed some data types so it was easy to display them

--- ingest_data.py
+++ ingest_data.py
@@ -1,6 +1,11 @@
 import json
 from elasticsearch import Elasticsearch, helpers
+from datetime import datetime
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
+nltk.download('vader_lexicon')
+sid = SentimentIntensityAnalyzer()
   
 #load api key
 f = open("/Users/tin/Documents/secrets/es_creds.json")
@@ -19,18 +24,26 @@ tweets = json.load(tweet_file_obj)
 
 for key in tweets:
     print("There are {} tweets to ingest".format(len(tweets[key])))
-
+    
+    for tweet in tweets[key]:
+        tweet['date'] = datetime.strptime(tweet['date'], '%Y-%m-%d %H:%M:%S')
+        tweet["sentiment"] = sid.polarity_scores(tweet["content"])
+        tweet["retweets"] = int(tweet["retweets"])
+        tweet["favorites"] = int(tweet["favorites"])
+        
 def gendata(tweets):
     for tweet in tweets["tweets"]:
         yield {
-            "_index" : "trumptweets",
+            "_index" : "trumptweetsentiment",
             "_source" : tweet
         }
         
 
-def bulk_ingest(tweets):
+def bulk_ingest(tweets_to_ingest):
     try:
-        response = helpers.bulk(es, gendata(buckets))
+        response = helpers.bulk(es, gendata(tweets_to_ingest))
         print("[+] Succesfully ingested {} tweets".format(response[0]))
     except Exception as e:
         print("[-] Error in bulk ingest: {}".format(e))
+
+bulk_ingest(tweets)
